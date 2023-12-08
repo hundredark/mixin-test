9a7da988d1ed174ba603bcefc8f69f2bdfc090fb
hundredark <hundredark@gmail.com>
2023-12-08 19:00:42 +0800

    Get all assets

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,7 +19,7 @@ const useGetContext  = () => {
   result.value = client.getMixinContext();
 };
 const useGetAssets = () => {
-  client.getAssets(["c6d0c728-2624-429b-8e0d-d9d19b6592fa", "43d61dcd-e413-450d-80b8-101d5e903357"], (res) => {
+  client.getAssets([], (res) => {
     result.value = res
   })
 };
